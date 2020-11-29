9bf5353cb1747697667c69fcfbe88c588a1dc5c9
faruk5266 <farukhussain5266@gmail.com>
2020-11-29 19:08:07 +0600

    never give up

---
+++ src/Components/Home/Partners/Partners.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Row, Col,Container} from 'react-bootstrap'
+// import {Container} from 'react-bootstrap/Container';
+import airbnb from '../../../images/logos/airbnb.png'
+import google from '../../../images/logos/google.png'
+import netflix from '../../../images/logos/netflix.png'
+import uber from '../../../images/logos/uber.png'
+import slack from '../../../images/logos/slack.png'
+const Partners = () => {
+    return (
+        <div>
+            <Container className='my-5'>
+                <Row xs={12} className='justify-content-around align-items-center '>
+                    <Col className='text-center' xs={6} md={2}><img style={{width:'100px'}} src={slack} alt=""/></Col>
+                    <Col className='text-center' xs={6} md={2}><img style={{width:'100px'}} src={google} alt=""/></Col>
+                    <Col className='text-center' xs={6} md={2}><img style={{width:'100px'}} src={uber} alt=""/></Col>
+                    <Col className='text-center' xs={6} md={2}><img style={{width:'100px'}} src={netflix} alt=""/></Col>
+                    <Col className='text-center' xs={6} md={2}><img style={{width:'100px'}} src={airbnb} alt=""/></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Partners;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdNS1zd3_SfreDPazY5PVu7r1qySzPVQM",
+    authDomain: "creative-agency-b038d.firebaseapp.com",
+    databaseURL: "https://creative-agency-b038d.firebaseio.com",
+    projectId: "creative-agency-b038d",
+    storageBucket: "creative-agency-b038d.appspot.com",
+    messagingSenderId: "257665840504",
+    appId: "1:257665840504:web:a50fddbcebea2fcb53c48d"
+  };
+  export default firebaseConfig;
